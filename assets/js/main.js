a94365bcb5ef651c069e622f626045f382fcdaee
Viktoriya <kapitonenko.vick@ya.ru>
2018-11-07 20:23:24 +0300

    Consulting: center menu, add right side of header, fix margin for left

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function changePadding() {
+  const container = document.querySelector('.consulting__container');
+  const headerLeft = document.querySelector('.header__left');
+  
+  const margin = getComputedStyle(container).marginLeft;
+  headerLeft.style.marginLeft = margin;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  changePadding();
+});
+
+window.onresize = function() {
+  changePadding();
+}
