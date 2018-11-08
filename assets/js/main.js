d831fb3284b2f77abaa7c9f2d88c92eff4f95573
Viktoriya <kapitonenko.vick@ya.ru>
2018-11-08 11:35:44 +0300

    Consulting: change style button, add work for partners

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
 window.onresize = function() {
   changePadding();
 }
+
+// Sliders
+const sliderDots = document.querySelectorAll('.carousel-btns__btn');
+let currentSlide = 0;
+let slides = document.querySelectorAll('.partners__list');
+
+const goToSlide = (n) => {
+  currentSlide = n;
+  const activeSlide = document.getElementsByClassName('slide_visible')[0];
+  activeSlide.classList.remove('slide_visible');
+  slides[currentSlide].classList.add('slide_visible');
+
+  const activeDot = document.getElementsByClassName('carousel-btns__btn_active')[0];
+  activeDot.classList.remove('carousel-btns__btn_active');
+  sliderDots[currentSlide].classList.add('carousel-btns__btn_active');
+};
+
+for(let i = 0; i < sliderDots.length; i++) {
+  sliderDots[i].addEventListener('click', () => {
+    goToSlide(i);
+  });
+}
